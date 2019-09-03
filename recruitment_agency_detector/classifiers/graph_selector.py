fix input with wrong shape to dynamic_rnn

recruitment_agency_detector/classifiers/graph_selector.py
@@ -105,16 +105,19 @@ class GraphSelector:
             initializer=embedding_initializer,
             trainable=False
         )
-        #rnn_layers = []
-        #for _ in range(self.config['lstm']['nr_layers']):
-        #    cell = tf.nn.rnn_cell.BasicLSTMCell(self.config['lstm']['hidden_size'])
-        #    rnn_layers.append(cell)
+        rnn_layers = [
+            tf.compat.v1.nn.rnn_cell.LSTMCell(self.config['lstm']['hidden_size'])
+            for _ in range(self.config['lstm']['nr_layers'])
+        ]
 
-        #multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(rnn_layers)
-        cell = tf.nn.rnn_cell.BasicLSTMCell(self.config['lstm']['hidden_size'])
-        outputs, final_state = tf.nn.dynamic_rnn(
-            cell, input_layer, sequence_length=input['len'], dtype=tf.float32)
+        multi_rnn_cell = tf.compat.v1.nn.rnn_cell.MultiRNNCell(rnn_layers)
+        outputs, final_state = tf.compat.v1.nn.dynamic_rnn(
+            cell=multi_rnn_cell,
+            inputs=input_layer,
+            sequence_length=input['len'],
+            dtype=tf.float32
+        )
 
-        #outputs = final_state.h
-        logits = tf.layers.dense(inputs=outputs[-1], units=2)
+        final_outputs = tf.transpose(outputs, [1,0,2])[-1]
+        logits = tf.layers.dense(inputs=final_outputs, units=2)
         return logits

recruitment_agency_detector/classifiers/tf_classifier.py
@@ -164,13 +164,13 @@ class TFClassifier:
         loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
 
         metric_ops = {
-            "accuracy": tf.metrics.accuracy(labels, predictions['classes']),
-            "auc": tf.metrics.auc(labels, predictions['classes'])
+            "accuracy": tf.compat.v1.metrics.accuracy(labels, predictions['classes']),
+            "auc": tf.compat.v1.metrics.auc(labels, predictions['classes'])
         }
 
         for metric_type, metric_op in metric_ops.items():
             # v[1] is the update op of the metrics object
-            tf.summary.scalar(metric_type, metric_op[1])
+            tf.compat.v1.summary.scalar(metric_type, metric_op[1])
 
         if mode == tf.estimator.ModeKeys.EVAL:
             return tf.estimator.EstimatorSpec(
@@ -180,10 +180,10 @@ class TFClassifier:
 
         # Configure the Training Op (for TRAIN mode)
         if mode == tf.estimator.ModeKeys.TRAIN:
-            optimizer = tf.train.AdamOptimizer(self.config['learning_rate'])
+            optimizer = tf.compat.v1.train.AdamOptimizer(self.config['learning_rate'])
             train_op = optimizer.minimize(
                 loss=loss,
-                global_step=tf.train.get_global_step())
+                global_step=tf.compat.v1.train.get_global_step())
             return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
         else:
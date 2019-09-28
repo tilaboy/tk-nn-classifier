d07a493ba573c383ec0e079125f4a249882750f5
chao <chaoli.job@gmail.com>
2019-09-28 03:42:02 +0200

    added label mapping as part of the data_loader package

--- recruitment_agency_detector/data_loader/__init__.py
+++ recruitment_agency_detector/data_loader/__init__.py
@@ -1,7 +1,7 @@
 from .data_encoder import DataEncoder
-from .data_reader import get_spacy_data, get_data_with_details, tokenize
+from .data_reader import get_spacy_data, get_data_with_details
 from .word_vector import WordVector
-
+from .tokenizer import tokenize
 
 __all__ = [
     'DataEncoder',

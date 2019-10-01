30c3a771a376f34a503546b5dc0bf1247d518c18
chao <chaoli.job@gmail.com>
2019-10-01 08:49:46 +0200

    removed not used tests

--- tests/test_classifier_utils.py
+++ tests/test_classifier_utils.py
@@ -1,7 +1,6 @@
 """unit tests for classifier utils functions"""
 import os
 from unittest import TestCase
-from recruitment_agency_detector.classifiers.utils import _max_dict_value
 from recruitment_agency_detector.classifiers.utils import TrainHelper
 
 class ClassifierUtilTestCases(TestCase):
@@ -16,67 +15,16 @@ class ClassifierUtilTestCases(TestCase):
             {'a': 0.1, 'b': 0.3, 'c': 0.6},
             {'a': 0, 'b': 0.9, 'c': 0.1}
         ]
-        self.assertEqual(_max_dict_value(cats_dict), ['a', 'c', 'b'])
+        self.assertEqual(TrainHelper.max_dict_value(cats_dict), ['a', 'c', 'b'])
 
 
-    def test_score(self):
-        eval = [
-            {'yes': 0.7,'no': 0.3},
-            {'yes': 0.9,'no': 0.1},
-            {'yes': 0.5,'no': 0.5},
-            {'yes': 0.3,'no': 0.7},
-            {'yes': 0.8,'no': 0.2},
-            {'yes': 0.1,'no': 0.9}
-        ]
-
-        gold = [
-            {'yes': 1.0,'no': 0.0},
-            {'yes': 0.0,'no': 1.0},
-            {'yes': 1.0,'no': 0.0},
-            {'yes': 0.0,'no': 1.0},
-            {'yes': 1.0,'no': 0.0},
-            {'yes': 0.0,'no': 1.0}
-
-        ]
-
-        score = self.train_helper._evaluate_score(eval, gold)
-        self.assertAlmostEqual(score["precision"], 0.75, places=2)
-        self.assertAlmostEqual(score["recall"], 1.00, places=2)
-        self.assertAlmostEqual(score["f1"], 0.857, places=2)
-
-
-    def test_confusion(self):
-        eval = [
-            {'yes': 0.7,'no': 0.3},
-            {'yes': 0.9,'no': 0.1},
-            {'yes': 0.5,'no': 0.5},
-            {'yes': 0.3,'no': 0.7},
-            {'yes': 0.8,'no': 0.2},
-            {'yes': 0.1,'no': 0.9}
-        ]
-
-        gold = [
-            {'yes': 1.0,'no': 0.0},
-            {'yes': 0.0,'no': 1.0},
-            {'yes': 1.0,'no': 0.0},
-            {'yes': 0.0,'no': 1.0},
-            {'yes': 1.0,'no': 0.0},
-            {'yes': 0.0,'no': 1.0}
-
-        ]
-
-        conf_matrix = self.train_helper._evaluate_confusion_matrix(eval, gold)
-        self.assertEqual(conf_matrix.iloc[0,0], 2)
-        self.assertEqual(conf_matrix.iloc[0,1], 1)
-        self.assertEqual(conf_matrix.iloc[1,0], 0)
-        self.assertEqual(conf_matrix.iloc[1,1], 3)
 
     def test_confusion_binary(self):
         eval = [1,1,1,0,1,0]
         gold = [1,0,1,0,1,0]
 
         conf_matrix = \
-                self.train_helper._evaluate_confusion_matrix_binary_class(
+                self.train_helper._evaluate_confusion_matrix(
                         eval, gold)
         self.assertEqual(conf_matrix.iloc[0,0], 2)
         self.assertEqual(conf_matrix.iloc[0,1], 1)

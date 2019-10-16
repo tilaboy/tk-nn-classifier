f3c92da9bfa579f4cde8abfb88658294177439a0
chao <chaoli.job@gmail.com>
2019-10-16 17:08:23 +0200

    change the return of the tf.data_reader

--- recruitment_agency_detector/data_loader/tf_data_reader.py
+++ recruitment_agency_detector/data_loader/tf_data_reader.py
@@ -2,6 +2,7 @@ from .data_reader import DataReader
 
 
 class TFDataReader(DataReader):
+
     def get_data(self, data_path):
         data_set = self.get_data_set(data_path)
         texts, labels = zip(*data_set)
@@ -9,4 +10,5 @@ class TFDataReader(DataReader):
         cats = [
             int(self.label_mapper.class_id(label))
             for label in labels]
-        return list(zip(texts, cats))
+        
+        return texts, cats

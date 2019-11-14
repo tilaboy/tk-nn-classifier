9bea4e7716477c1931a4f644a6ed8eb1d0869625
chao <chaoli.job@gmail.com>
2019-11-14 10:40:59 +0100

    update the requirements

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ VERSION = os.environ.get("TK_NN_CLASSIFIER", '0.0.0')
 INSTALL_REQUIRES = [
     "tensorflow>=1.14.0",
     "numpy>=1.16.0",
-    "spacy>=2.1.8",
     "xml-miner>=0.0.4",
-    "en-core-web-sm>=2.1.0",
     "tk-preprocessing>=0.0.4"
 ]
 

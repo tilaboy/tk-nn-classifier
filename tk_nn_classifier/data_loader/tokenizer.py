8e2a5b57730ad56fecf2e5ea404ee8e880375faf
chao <chaoli.job@gmail.com>
2019-11-30 23:34:19 +0100

    added a tokenizer module and a test for it

--- tk_nn_classifier/data_loader/tokenizer.py
+++ tk_nn_classifier/data_loader/tokenizer.py
@@ -1,6 +1,7 @@
 import re
 from easy_tokenizer.normalizer import normalize_chars
 from easy_tokenizer.tokenizer import Tokenizer
+from easy_tokenizer.patterns import Patterns
 
 HAS_TOKEN_REGEXP = re.compile(r'\w')
 TOKEN_REGEXP = re.compile(r'\w+|[^\w\s]+')
@@ -13,4 +14,21 @@ def tokenize(string):
     except NameError:
         TOKENIZER = Tokenizer()
 
-    return list(TOKENIZER.tokenize(string))
+    return [_norm_token(token) for token in TOKENIZER.tokenize(string)]
+
+
+def _norm_token(token):
+    norm_token = token.upper()
+    if Patterns.URL_RE.fullmatch(token):
+        norm_token = 'xxURLxx'
+    elif Patterns.EMAIL_RE.fullmatch(token):
+        norm_token = 'xxEMAILxx'
+    elif Patterns.DOMAIN_RE.fullmatch(token):
+        norm_token = 'xxAT_DOMAINxx'
+    elif Patterns.YEAR_RE.fullmatch(token):
+        norm_token = 'xxYEARxx'
+    elif Patterns.EMAIL_RE.fullmatch(token):
+        norm_token = 'xxEMAILxx'
+
+    norm_token = Patterns.DIGIT_RE.sub('1', norm_token)
+    return norm_token

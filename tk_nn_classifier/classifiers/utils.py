efb1f09d222ffd00da5f3853611ec9496f4730f9
chao <chaoli@textkernel.nl>
2019-12-27 08:00:53 +0100

    use creation time for latest export model

--- tk_nn_classifier/classifiers/utils.py
+++ tk_nn_classifier/classifiers/utils.py
@@ -1,6 +1,25 @@
 import os
+import platform
 import pandas as pd
 from .. import LOGGER
+
+
+def creation_date(path_to_file):
+    """
+    Try to get the date that a file was created, falling back to when it was
+    last modified if that isn't possible.
+    See http://stackoverflow.com/a/39501288/1709587 for explanation.
+    """
+    if platform.system() == 'Windows':
+        return os.path.getctime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return stat.st_birthtime
+        except AttributeError:
+            # We're probably on Linux. No easy way to get creation dates here,
+            # so we'll settle for when its content was last modified.
+            return stat.st_mtime
 
 
 class FileHelper:
@@ -14,7 +33,7 @@ class FileHelper:
                 for name in os.listdir(model_path)
         ]
         model_path = max(model_names,
-                         key=lambda x: int(os.stat(x).st_birthtime))
+                         key=lambda x: int(creation_date(x)))
         return model_path
 
 

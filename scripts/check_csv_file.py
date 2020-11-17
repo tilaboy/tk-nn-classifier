4ca87c9af92874df9938e927c66d3c8e26b212c5
chao <chaoli.job@gmail.com>
2020-11-17 15:24:28 +0800

    small update on the script

--- scripts/check_csv_file.py
+++ scripts/check_csv_file.py
@@ -7,12 +7,16 @@ def _prepare_input_text(text):
     return text
 
 def check_staffing_agency_csv(data_path):
+    total_counts = dict()
     with open(data_path, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             text = _prepare_input_text(row['full_text'])
+            adv_type = row['advertiser_type']
+            total_counts[adv_type] = total_counts.get(adv_type, 0) + 1
             if len(text) < 10 or row['advertiser_type'] not in ['1', '2']:
                 print(row)
+    return total_counts
 
 def get_args():
     '''get arguments'''
@@ -25,4 +29,7 @@ def get_args():
 
 if __name__ == '__main__':
     args = get_args()
-    check_staffing_agency_csv(args.csv_file)
+    total_counts = check_staffing_agency_csv(args.csv_file)
+
+    for category in total_counts:
+        print("type {}: in total {} items".format(category, total_counts[category]))

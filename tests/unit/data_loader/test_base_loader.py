0dc2cf13748a6e9c3760e40c5c07fe21eadbfba3
chao <chaoli.job@gmail.com>
2020-12-08 17:09:58 +0800

    added test func for base loader

--- tests/unit/data_loader/test_base_loader.py
+++ tests/unit/data_loader/test_base_loader.py
@@ -8,6 +8,21 @@ class BaseLoaderTestCases(TestCase):
     def setUp(self):
         self.config= {
             "max_lines": 5,
+            "trxml_fields": {
+                "features": "sec_vacancy.0.sec_vacancy",
+                "class": "derived_vac_intermediary.0.derived_vac_intermediary",
+                "doc_id": "Document.0.correlationid",
+                "extra": ["derived_org_name.0.derived_org_name",
+                    "derived_norm_url.0.derived_norm_url"]
+            },
+
+            "csv_fields": {
+                "features": "full_text",
+                "class": "advertiser_type",
+                "doc_id": "posting_id",
+                "extra": ["organization_name", "source_type", "source_website"]
+            },
+
             "datasets": {}
         }
 
@@ -23,10 +38,35 @@ class BaseLoaderTestCases(TestCase):
                          input_text
                         )
 
-    def test_flatten_array(self):
+    def test_trxml_train_fields(self):
         base_loader = BaseLoader(self.config)
-        mixed_array = [[0,1,2],3,4,5,[6,[7,8]],9]
+        all_fields = ['sec_vacancy.0.sec_vacancy',
+                      'derived_vac_intermediary.0.derived_vac_intermediary',
+                      'Document.0.correlationid',
+                      'derived_org_name.0.derived_org_name',
+                      'derived_norm_url.0.derived_norm_url']
+
         self.assertEqual(
-                list(base_loader._iter_flatten(mixed_array)),
-                [0,1,2,3,4,5,6,7,8,9]
+            base_loader._train_fields('trxml_fields'),
+            all_fields[0:2]
+        )
+
+        self.assertEqual(
+            base_loader._detail_fields('trxml_fields'),
+            all_fields
+        )
+
+    def test_csv_train_fields(self):
+        base_loader = BaseLoader(self.config)
+        all_fields = ['full_text', 'advertiser_type',
+                      'posting_id', 'organization_name',
+                      'source_type', 'source_website']
+        self.assertEqual(
+            base_loader._train_fields('csv_fields'),
+            all_fields[0:2]
+        )
+
+        self.assertEqual(
+            base_loader._detail_fields('csv_fields'),
+            all_fields
         )

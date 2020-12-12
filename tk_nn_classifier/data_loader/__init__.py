a5defa06c177cd0b030ac139b67b70048f5845cf
chao <chaoli.job@gmail.com>
2020-12-12 23:18:18 +0800

    added general spliting functions

--- tk_nn_classifier/data_loader/__init__.py
+++ tk_nn_classifier/data_loader/__init__.py
@@ -1,21 +1,31 @@
 import os
+import sys
 from ..exceptions import FileTypeError
 from .csv_loader import CSVLoader, split_csv_file
 from .trxml_loader import TRXMLLoader, split_trxml_set
+from .data_utils import file_ext
 
-def _select_data_reader(field_config, data_path):
+def _data_type(data_path):
+    data_type = None
     if os.path.isdir(data_path):
         # TODO: check at least on trxml exist
-        data_reader = TRXMLLoader(field_config)
+        data_type = 'TRXML'
     elif os.path.isfile(data_path):
-        _, file_ext = os.path.splitext(data_path)
-        if file_ext in ['.csv', '.tsv']:
-            data_reader = CSVLoader(field_config)
+        file_extension = file_ext(data_path)
+        if file_extension in ['csv', 'tsv']:
+            data_type = 'CSV'
         else:
             raise FileTypeError(file_ext)
     else:
         raise FileNotFoundError(f'{data_path} not found')
-    return data_reader
+    return data_type
+
+
+def _select_data_reader(field_config, data_path):
+    data_type = _data_type(data_path)
+    loader_class = getattr(sys.modules[__name__], data_type + 'Loader')
+    return loader_class(field_config)
+
 
 def load_data_set(field_config, data_path, train_mode=True):
     data_reader = _select_data_reader(field_config, data_path)
@@ -23,3 +33,11 @@ def load_data_set(field_config, data_path, train_mode=True):
         return data_reader.load_train_data(data_path)
     else:
         return data_reader.load_detail_data(data_path)
+
+def split_data_set(data_path, ratio, des, rand_seed):
+    data_type = _data_type(data_path)
+    if data_type == 'TRXML':
+        train_set, eval_set = split_trxml_set(data_path, ratio, des, rand_seed)
+    elif data_type == 'CSV':
+        train_set, eval_set = split_csv_file(data_path, ratio, des, rand_seed)
+    return train_set, eval_set
